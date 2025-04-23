c59cf2f6a61da7cd45354c23ee2652cb050fa368
Luke Policinski <luke@lukepolo.com>
2025-04-23 09:02:08 -0400

    chore: get rid of warning

--- components/PlayerElo.vue
+++ components/PlayerElo.vue
@@ -11,18 +11,19 @@
 export default {
   props: {
     elo: {
-      type: Number,
-      required: true,
+      type: String,
+      required: false,
     },
   },
   computed: {
     eloColorClass(): string {
-      if (this.elo >= 30) return "text-[#EB4B4B]";
-      if (this.elo >= 25) return "text-[#D22CE6]";
-      if (this.elo >= 20) return "text-[#FED700]";
-      if (this.elo >= 15) return "text-[#8846FF]";
-      if (this.elo >= 10) return "text-[#4B69FF]";
-      if (this.elo >= 5) return "text-[#5E98D7]";
+      const elo = Number(this.elo);
+      if (elo >= 30) return "text-[#EB4B4B]";
+      if (elo >= 25) return "text-[#D22CE6]";
+      if (elo >= 20) return "text-[#FED700]";
+      if (elo >= 15) return "text-[#8846FF]";
+      if (elo >= 10) return "text-[#4B69FF]";
+      if (elo >= 5) return "text-[#5E98D7]";
       return "text-[#B1C3D9]";
     },
   },
